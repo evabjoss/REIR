7671df8c4cea21e1a34cd6cbe073d40f28f9dfa2
mariamjoll1 <mariahrafns@gmail.com>
2020-09-26 21:45:46 +0000

    sorry eg var ekki á sér branchi :s

---
+++ src/S2/test.java
@@ -0,0 +1,20 @@
+package S2;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class test {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
